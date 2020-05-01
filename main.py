c8b944b457d811b7cdc2c90aa80ce3eb76d85e6b
Batuhan Aslan <daghostyboi@gmail.com>
2020-05-01 12:29:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#Thanks for looking to my project! This has so many effort on it, Please leave a like :)
+#All made by me
+
+import time
+
+print("Welcome to ChatBot v0.1 Pre-Alpha!")
+time.sleep(01.5)
+print("This project is still work in progress, please contact me if have an idea or bug!")
+time.sleep(1.5)
+
+#Main codes
+
+while True:
+ answer= input("Type something..\n")
+  
+ if answer ==  "Hello" or answer ==  "Hi":
+   print("Hi!")
+   
+ elif answer == "How are you":
+     print("I'm fine, thanks!")
+
+ time.sleep(2)         
+    
